6156d7a8436c3c92e52b93c4bbd935b148d1a16b
lioraft <lioraftabi@gmail.com>
2023-05-27 18:47:13 +0300

    added maven dependencies, and user32 interface

---
+++ src/User32.java
@@ -0,0 +1,11 @@
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+import com.sun.jna.win32.W32APIOptions;
+
+
+/* this is the interface that will be used to call the SystemParametersInfo function from the user32.dll library */
+public interface User32 extends Library {
+    User32 INSTANCE = (User32) Native.loadLibrary("user32", User32.class, W32APIOptions.DEFAULT_OPTIONS);
+
+    int SystemParametersInfo(int uAction, int uParam, String lpvParam, int fuWinIni);
+}
